3e7f93a03a940d74bcdb0b903ffe332b7c434fdc
ngdangkietse <kietnguyen17052001@gmail.com>
2025-02-01 06:49:19 +0000

    chore: regenerate files

--- generated/integration/strava_service.pb.validate.go
+++ generated/integration/strava_service.pb.validate.go
@@ -59,6 +59,35 @@ func (m *IntegrateStravaAccountReq) validate(all bool) error {
 
 	// no validation rules for UserId
 
+	if all {
+		switch v := interface{}(m.GetStrava()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, IntegrateStravaAccountReqValidationError{
+					field:  "Strava",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, IntegrateStravaAccountReqValidationError{
+					field:  "Strava",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetStrava()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return IntegrateStravaAccountReqValidationError{
+				field:  "Strava",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return IntegrateStravaAccountReqMultiError(errors)
 	}
